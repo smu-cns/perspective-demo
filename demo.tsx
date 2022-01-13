1aff15219721f3fe4f383864d8809b1d65b2160c
Stefan <80399264+smu-cns@users.noreply.github.com>
2022-01-13 11:18:57 +0100

    make/open close work

--- demo.tsx
+++ demo.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button/Button';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button/Button";
 
-import { Allotment } from 'allotment';
-import 'allotment/dist/style.css';
-import { openPane, Panel, useAppDispatch, useAppSelector } from './store';
+import { Allotment } from "allotment";
+import "allotment/dist/style.css";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import { openPanel, closePanel, Panel } from "./perspectiveSlice";
 
 export default function SimpleContainer() {
   const dispatch = useAppDispatch();
@@ -16,13 +17,16 @@ export default function SimpleContainer() {
     <React.Fragment>
       <CssBaseline />
       <Container>
-        <Button onClick={(e) => dispatch(openPane)}>Add</Button>
-        <Box sx={{ bgcolor: '#cfe8fc', height: '80vh' }}>
+        <Button onClick={(e) => dispatch(openPanel())}>Open Panel</Button>
+        <Box sx={{ bgcolor: "#cfe8fc", height: "80vh" }}>
           <Allotment snap>
             {panels.map((p) => (
               <Allotment.Pane key={p.id}>
-                <div style={{ height: '100%', backgroundColor: '#ccc' }}>
-                  {p.label} <Button onClick={(e) => closePane(p.id)}>X</Button>
+                <div style={{ height: "100%", backgroundColor: "#ccc" }}>
+                  {p.label}{" "}
+                  <Button onClick={(e) => dispatch(closePanel(p.id))}>
+                    Close Panel
+                  </Button>
                 </div>
               </Allotment.Pane>
             ))}
